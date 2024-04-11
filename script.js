f2f47f8d8be5648ff18354ac97954b324e42c1cd
Eduardo2006g <101937977+Eduardo2006g@users.noreply.github.com>
2024-04-11 10:32:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-function pedirNome() {
-  // Usando o prompt para pedir o nome
-  var nome = prompt("Digite seu nome:");
+function pedirNome(x) {
+    // Usando o prompt para pedir o nome
+    var nome = prompt("Digite seu nome:");
+  
+    // Exibindo o nome em um alert
+    alert("Olá, " + nome + "!");
+    
+    var div = document.createElement("div");
+    div.className = "nome";
+    div.textContent = nome;
+    
+    
+  if (x == true) {
+    document.getElementById('sobre-colA').appendChild(div);
+  }
+  else {
+    document.getElementById('sobre-colP').appendChild(div);
+  }
+  
+  }
 
-  // Exibindo o nome em um alert
-  alert("Olá, " + nome + "!");
-}
+function mostrar(x) {
+  var elementA = document.getElementById('sobresA');
+  var elementP = document.getElementById('sobresP');
+  if(x == true) {
+    elementA.style.display = "flex";
+    elementP.style.display = "none";
+  }
+  else{elementP.style.display = "flex"
+  elementA.style.display = "none";};
+    
+} 
+
